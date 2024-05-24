87450212a0dd54aab33a21673ffb660a8d6abd9b
ali emad <aliemadaboshosha13@gmail.com>
2024-05-24 20:15:45 +0300

    create the header and footer templates with static data 24/5/2024 8:15 PM

--- src/app/components/main-components/top-header/top-header.component.ts
+++ src/app/components/main-components/top-header/top-header.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faBars, faChevronDown, faUserTie, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-top-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './top-header.component.html',
   styleUrl: './top-header.component.scss'
 })
 export class TopHeaderComponent {
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faBars, faChevronDown, faUserTie, faShoppingCart);
+  }
 
 }
