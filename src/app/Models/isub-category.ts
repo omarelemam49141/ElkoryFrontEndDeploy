6129b03f3adf9fcb762563bd08384aa44f9f5fb2
ali emad <aliemadaboshosha13@gmail.com>
2024-06-28 09:19:00 +0300

    before merging with main to get the mew cart model

--- src/app/Models/isub-category.ts
+++ src/app/Models/isub-category.ts
@@ -1,13 +1,26 @@
 export interface ISubCategory {
     subCategoryId: number;
     name: string;
-    categories: {
-        categoryId: number;
-        name: string;
+ 
         values: {
             value: string;
             imageId: string;
             imageUrl: string;
         }[];
-    }[]
+    
 }
+
+
+// export interface ISubCategory {
+//     subCategoryId: number;
+//     name: string;
+//     categories: {
+//         categoryId: number;
+//         name: string;
+//         values: {
+//             value: string;
+//             imageId: string;
+//             imageUrl: string;
+//         }[];
+//     }[]
+// }
