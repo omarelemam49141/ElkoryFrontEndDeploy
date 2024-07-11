152f678b68bb00d8ec45c1247e08aa1fa56042e7
ali emad <aliemadaboshosha13@gmail.com>
2024-07-11 11:56:10 +0300

    update the currancy pipe to be suitable with egyptian pounds

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+
+import localeArEg from '@angular/common/locales/ar-EG';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeArEg, 'ar-EG');
+
+
+import { enableProdMode } from '@angular/core';
+enableProdMode();
+
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
